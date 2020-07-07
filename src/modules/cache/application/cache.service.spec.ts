700e69ccdc424bd47b4d91f90cf1703a0ec7c11e
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-06 20:51:29 -0500

    feat(Testing): new Set, Get ,Gets unit testing

--- src/modules/cache/application/cache.service.spec.ts
+++ src/modules/cache/application/cache.service.spec.ts
@@ -12,7 +12,32 @@ describe('CacheService', () => {
     service = module.get<CacheService>(CacheService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return a hello workd', () => {
+    expect(service.getHello()).toEqual('Hello World!');
+  });
+  
+  describe('Cache Functions', () => {
+    //Testing a set key function
+    it('Should return a true if the object was saved', async () => {
+      const result = true;
+      //jest.spyOn(service, 'Cache Functions').mockImplementation(() => result);
+      expect(await service.setKey("nombre","Anderson Laverde",3600)).toBe(result);
+    });
+    // Testing a get ket value function
+    it('Should return a value of a object', async () => {
+      const result = "Anderson Laverde";
+      //jest.spyOn(service, 'Cache Functions').mockImplementation(() => result);
+      expect(await service.getKeyValue("nombre")).toBe(result);
+    });
+    //
+    it('Should return a full object stored in memory', async () => {
+      const result = 'Anderson Laverde'
+      //jest.spyOn(service, 'Cache Functions').mockImplementation(() => result);
+      service.getsKeyValue("nombre")
+        .then(response =>{
+          expect(response["nombre"]).toBe('Anderson Laverde');  
+          expect(typeof response.cas).toBe("string") ;
+        });
+    });
   });
 });
